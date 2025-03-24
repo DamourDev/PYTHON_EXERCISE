9acc3b95a44d263430b08cac08fd6c45b3da1b2b
Your Name <nkurunzizajeandamour00@gmail.com>
2025-03-24 09:48:45 +0200

    ODD NUMBERS BETWEEN A AND B, B INCLUDED

--- Exercises.py
+++ Exercises.py
@@ -1,4 +1,14 @@
-string = input("Enter a string you want : ")
-reference = str.maketrans("kajipotefuKAJIPOTEFU","akijopetufAKIJOPETUF")
-text = string.translate(reference)
-print(text)
+a = int(input("Enter a lower number:")) 
+b = int(input("Enter a higher number:"))
+def OddNumbers():
+    for num in range (a,b+1):
+        if num %2 != 0 :
+            # 
+            print(num)
+            num+=2
+print(f"Odd numbers between {a} and {b+1} are :" )    
+OddNumbers()
+
+
+
+
