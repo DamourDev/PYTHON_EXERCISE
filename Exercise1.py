74da91424998c3e9f4ea35588e26ae942e8a8037
Your Name <nkurunzizajeandamour00@gmail.com>
2025-03-24 10:58:26 +0200

    POWER PROGRAM

--- Exercise1.py
+++ Exercise1.py
@@ -1,10 +1,18 @@
-a = int(input("Enter a lower number:")) 
-b = int(input("Enter a higher number:"))
-def OddNumbers():
-    for num in range (a,b+1):
-        if num %2 != 0 :
-            # 
-            print(num)
-            num+=2
-print(f"Odd numbers between {a} and {b+1} are :" )    
-OddNumbers()
+# a = int(input("Enter a lower number:")) 
+# b = int(input("Enter a higher number:"))
+# def OddNumbers():
+#     for num in range (a,b+1):
+#         if num %2 != 0 :
+#             # 
+#             print(num)
+#             num+=2
+# print(f"Odd numbers between {a} and {b+1} are :" )    
+# OddNumbers()
+
+# program to return first number to the powe of the second number
+
+a = int(input("Enter a first number:"))
+b = int(input("Enter a second number:"))
+def power(a,b):
+    return b**a
+print(f"{b} power {a} is {power(a,b)}")
--- Exercises.py
+++ Exercises.py
@@ -1,13 +1,17 @@
-a = int(input("Enter a lower number:")) 
-b = int(input("Enter a higher number:"))
-def OddNumbers():
-    for num in range (a,b+1):
-        if num %2 != 0 :
-            # 
-            print(num)
-            num+=2
-print(f"Odd numbers between {a} and {b+1} are :" )    
-OddNumbers()
+# a = int(input("Enter a lower number:")) 
+# b = int(input("Enter a higher number:"))
+# def OddNumbers():
+#     for num in range (a,b+1):
+#         if num %2 != 0 :
+#             # 
+#             print(num)
+#             num+=2
+# print(f"Odd numbers between {a} and {b+1} are :" )    
+# OddNumbers()
+
+
+
+
 
 
 
